c82d0745d8d872f8d8b3ea149ae8a0552e46537b
JavierGodoyf <javiergodoyf91@gmail.com>
2023-11-29 09:24:20 -0300

    to GitHub

---
+++ estructuraSitioWeb.bat
@@ -0,0 +1,27 @@
+mkdir assets
+mkdir css
+mkdir js
+mkdir pages
+mkdir scss
+echo ^<!--Este archivo fue autogenerado via script de Javier Godoy Fernandez >> index.html
+echo https://github.com/JavierGodoyf >> index.html
+echo code wisely ^<3 --^> >> index.html
+echo ^<!DOCTYPE html^> >> index.html
+echo    ^<html lang="en"^> >> index.html
+echo    ^<head^> >> index.html
+echo        ^<meta charset="UTF-8"^> >> index.html
+echo        ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^> >> index.html
+echo        ^<title^>Document^</title^> >> index.html
+echo    ^</head^> >> index.html
+echo    ^<body^> >> index.html
+echo.   >> index.html
+echo    ^</body^> >> index.html
+echo ^</html^> >> index.html
+cd assets
+mkdir img
+mkdir video
+mkdir fonts
+mkdir audio
+cd ..
+cd css
+type nul > css/styles.css
